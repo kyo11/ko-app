b12761a088c855d41efea7c4e7128ae255f35e03
kyo11 <kyosuke_0911@outlook>
2020-07-31 00:58:04 +0900

    syu

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'messages/index'
   # resources :articles
-  root "messages#index"
+  root 'groups#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
