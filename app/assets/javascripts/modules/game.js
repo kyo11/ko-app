db737d1f31e264676bfa3933efa2c6f23e4ab6a7
kyo11 <kyosuke_0911@outlook>
2020-08-03 19:42:14 +0900

    syu

--- app/assets/javascripts/modules/game.js
+++ app/assets/javascripts/modules/game.js
@@ -1,38 +1,162 @@
-var p = document.getElementById('text');
-var textLists = [
-  'Hello World',
-  'This is my App',
-  'How are you?',
-  'Hello Hello'
-];
-var checkTexts = [];
+(function() {
+  'use strict';
 
-createText();
+  // //ビンゴの数字の数 75
+  var Bingo = (function(from, to) {
+  for(var tmp = []; from <= to; from++) {
+      tmp.push(('0' + from).slice(-2));
+      }
+      return tmp;
+  })(1, 75);
 
-function createText() {
-  var rnd = Math.floor(Math.random() * textLists.length);
+  var timers = [];
+  var results = [];
+  var stopCount = 0;
+  var isPlaying = false;
 
-  checkTexts = textLists[0].split('').map(function(value) {
-    var span = document.createElement('span');
+  //乱数によってセレクトされた数字
+  var bingoNum = 0;
 
-    span.textContent = value;
-    p.appendChild(span);
+  //bingoNumでセレクトされた数字をBingo配列から削除する変数
+  var delArrayNum = 0;
 
-    return span;
-});
-}
+  var panel1 = document.getElementById('panel1');
+  var panel10 = document.getElementById('panel10');
+  var btn0 = document.getElementById('btn0');
+  var spinButton = document.getElementById('spinButton');
+
+  var soundManager = SoundManager();
 
 
+  // ビンゴの結果エリアのレンダリング
+  var renderBingo = function(){
+      var fragment= document.createDocumentFragment();
+      var divWrapper;
+      Bingo.forEach(function(elem, index){
+          if( index % 15 === 0 ){
+              divWrapper = fragment.appendChild(document.createElement("div"));
+          }
+          var numDiv = divWrapper.appendChild(document.createElement("div"));
+          numDiv.className = "bingo";
+          numDiv.innerHTML = elem;
+      });
+      var result = document.getElementById("result");
+      result.appendChild(fragment);
+  };
+  renderBingo();
 
-document.addEventListener('keydown',keyDown);
+  //『SPIN』ボタン押下後の処理
+  spinButton.addEventListener('click', function() {
+      if (isPlaying) return;
 
+      soundManager.playDrum();
 
-function keyDown(e) {
-  if(e.key === checkTexts[0].textContent) {
-    checkTexts[0].className = 'add-blue';
+      isPlaying = true;
+      this.className = 'inactive';
+      btn0.className = 'btn';
 
-    checkTexts.shift();
+      panel1.className = 'panel';
+      panel10.className = 'panel';
 
-    if(!checkTexts.length) createText();
+      runSlot(0, panel1);
+      runSlot(1, panel10);
+
+  });
+
+  //『SPIN』ボタン押下後の処理
+  function runSlot(n, panel) {
+
+      var num = Bingo[Math.floor(Math.random() * Bingo.length)];
+      bingoNum = num;
+
+      //ランダムで表示されている数字を1のくらいと、10のくらいで分割して表示させる
+      var num1 = num.substr(0,1); 
+      var num10 = num.substr(1,2); 
+
+      //表示
+      panel1.innerHTML = num1;
+      panel10.innerHTML = num10;
+
+      timers[n] = setTimeout(function() {
+          runSlot(n, panel)
+      }, 25);
+
   }
-}
+
+  btn0.addEventListener('click', function() {
+      stopSlot(0, panel1 ,panel10, this);
+  });
+
+  //『STOP』ボタン押下後の処理
+  function stopSlot(n, panel1, panel10, btn) {
+      if (!isPlaying || results[n] !== undefined) return;
+      btn.className = 'btn inactive';
+
+      soundManager.stopDrum();
+      soundManager.playCymbal();
+
+      clearTimeout(timers[n]);
+      clearTimeout(timers[n+1]);
+
+      //ランダムで選択された数字bingoNumをBingo配列から選定
+      delArrayNum = Bingo.indexOf(bingoNum);
+      if(delArrayNum >= 0){
+          //Bingo配列から削除
+          Bingo.splice(delArrayNum,1);
+      }
+
+      stopCount++;
+
+      if (stopCount === 1) {
+
+          stopCount = 0;
+          spinButton.className = '';
+
+          var bingoDiv = document.querySelectorAll(".bingo");
+
+          // change color
+          bingoDiv[bingoNum-1].innerHTML;
+          bingoDiv[bingoNum-1].className = 'bingo unmatched';
+
+          // init
+          isPlaying = false;
+          spinButton.className = '';
+          timers = [];
+      }
+  }
+
+  //wavファイルを実行するメソッド
+  function SoundManager(){
+      var drum = document.getElementById('audio_drum');
+      var cymbal = document.getElementById('audio_cymbal');
+      return {
+        playDrum    : playDrum,
+        stopDrum    : stopDrum,
+        playCymbal  : playCymbal
+      }
+
+      function playDrum(){
+        if(drum == null){
+          return;
+        }
+        drum.currentTime = 0;
+        drum.play();
+      }
+
+      function stopDrum(){
+        if(drum == null){
+          return;
+        }
+        drum.pause();
+      }
+
+      function playCymbal(){
+        if(cymbal == null){
+          return;
+        }
+        cymbal.currentTime = 0;
+        cymbal.play();
+      }
+  }
+
+})();
